da20947cfb6881dc7db7e1ecca0990bade21afac
arozhdan <halala30@gmail.com>
2020-11-19 13:39:15 +0100

    makup upd

--- pages/index.vue
+++ pages/index.vue
@@ -2,61 +2,331 @@ import { Plank, Teachers, Whyus, Contactus, Faq, Promo } from '../.nuxt/componen
 <template>
   <div class="home-page">
     <Header />
-    <Advantages />
-    <Benefits />
-    <Wellcome />
-    <About />
+    <div class="advantages-wrapper">
+      <div class="advantage-item">
+        <div class="advantage-item-icon">
+          <img src="../assets/img/molecule.svg" alt="molecule" />
+        </div>
+        <h3 class="advantage-item-title">Индивидуальная программа</h3>
+        <p class="advantage-text">
+          Все уроки в NETschool прохоят индивидуально. Наша школа поможет вам
+          составить индивидуальный план обучения с преподавателем.
+        </p>
+      </div>
+      <div class="advantage-item">
+        <div class="advantage-item-icon">
+          <img src="../assets/img/schedule.svg" alt="molecule" />
+        </div>
+        <h3 class="advantage-item-title">Индивидуальная программа</h3>
+        <p class="advantage-text">
+          Все уроки в NETschool прохоят индивидуально. Наша школа поможет вам
+          составить индивидуальный план обучения с преподавателем.
+        </p>
+      </div>
+      <div class="advantage-item">
+        <div class="advantage-item-icon">
+          <img src="../assets/img/clock.svg" alt="molecule" />
+        </div>
+        <h3 class="advantage-item-title">Индивидуальная программа</h3>
+        <p class="advantage-text">
+          Все уроки в NETschool прохоят индивидуально. Наша школа поможет вам
+          составить индивидуальный план обучения с преподавателем.
+        </p>
+      </div>
+      <div class="advantage-item">
+        <div class="advantage-item-icon">
+          <img src="../assets/img/calc.svg" alt="molecule" />
+        </div>
+        <h3 class="advantage-item-title">Индивидуальная программа</h3>
+        <p class="advantage-text">
+          Все уроки в NETschool прохоят индивидуально. Наша школа поможет вам
+          составить индивидуальный план обучения с преподавателем.
+        </p>
+      </div>
+    </div>
+    <section class="benefits">
+      <div class="container">
+        <h2 class="section-title">Что мы Вам даем?</h2>
+        <p class="section-suptitle">
+          В нашем образовательном центре мы предоставляем множество преимуществ
+          для успешного обучения ваших детей, помогая при этом развивать их
+          аналитический склад ума, тврочество и поддерживая интерес.
+        </p>
+        <div class="benefits-inner row">
+          <div class="benefits-item col-md-4">
+            <div class="benefits-item-left">
+              <h3 class="benefits-item-title">Персональный ментор</h3>
+              <p class="benefits-item-text">
+                Нужен только компьютер и доступ к Интернету
+              </p>
+            </div>
+            <div class="benefits-item-icon">
+              <img src="../assets/img/management.svg" alt="" />
+            </div>
+          </div>
+          <div class="benefits-item col-md-4">
+            <div class="benefits-item-left">
+              <h3 class="benefits-item-title">Персональный ментор</h3>
+              <p class="benefits-item-text">
+                Нужен только компьютер и доступ к Интернету
+              </p>
+            </div>
+            <div class="benefits-item-icon">
+              <img src="../assets/img/reward.svg" alt="" />
+            </div>
+          </div>
+          <div class="benefits-item col-md-4">
+            <div class="benefits-item-left">
+              <h3 class="benefits-item-title">Персональный ментор</h3>
+              <p class="benefits-item-text">
+                Нужен только компьютер и доступ к Интернету
+              </p>
+            </div>
+            <div class="benefits-item-icon">
+              <img src="../assets/img/target.svg" alt="" />
+            </div>
+          </div>
+          <div class="benefits-item col-md-4">
+            <div class="benefits-item-left">
+              <h3 class="benefits-item-title">Персональный ментор</h3>
+              <p class="benefits-item-text">
+                Нужен только компьютер и доступ к Интернету
+              </p>
+            </div>
+            <div class="benefits-item-icon">
+              <img src="../assets/img/stat.svg" alt="" />
+            </div>
+          </div>
+          <div class="benefits-item col-md-4">
+            <div class="benefits-item-left">
+              <h3 class="benefits-item-title">Персональный ментор</h3>
+              <p class="benefits-item-text">
+                Нужен только компьютер и доступ к Интернету
+              </p>
+            </div>
+            <div class="benefits-item-icon">
+              <img src="../assets/img/rocket.svg" alt="" />
+            </div>
+          </div>
+          <div class="benefits-item col-md-4">
+            <div class="benefits-item-left">
+              <h3 class="benefits-item-title">Персональный ментор</h3>
+              <p class="benefits-item-text">
+                Нужен только компьютер и доступ к Интернету
+              </p>
+            </div>
+            <div class="benefits-item-icon">
+              <img src="../assets/img/hourglass.svg" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <div class="wellcome">
+      <div class="wellcome-inner container">
+        <h2 class="secondary-title secondary-title__wellcome">
+          Добро Пожаловать в LikeIT school!
+        </h2>
+        <p class="wellcome-text">
+          В нашем образовательном центре мы предоставляем множество преимуществ
+          для успешного обучения ваших детей, помогая при этом развивать их
+          аналитический склад ума, тврочество и поддерживая интерес.
+        </p>
+        <div class="btn-wrapper">
+          <button class="btn btn__secondary">Начать учиться</button>
+        </div>
+      </div>
+    </div>
+    <section class="about">
+      <h2 class="section-title">О нас</h2>
+      <p class="section-suptitle about-sup">
+        В LikeIT school наша миссия состоит в том, чтобы обеспечить превосходное
+        качество в дошкольных учреждениях, детских садах, до и после школьных
+        программ, а также в летних лагерях для семей, живущих в большей
+        местности.
+        <br /><br />
+        Здесь вы найдете исключительных учителей, учебную программу и учебную
+        среду, которая побуждает детей учиться, играть и исследовать в
+        безопасной, здоровой и благоприятной среде, способствующей творческому
+        мышлению.
+      </p>
+    </section>
     <Services />
-    <Plank />
-    <Teachers />
+    <div class="plank">
+      <div class="container">
+        <div class="row plank-inner">
+          <div class="col-md-9 plnak-content">
+            <div class="plank-img">
+              <img src="../assets/img/cookie2.svg" alt="" />
+            </div>
+            <div class="plnak-content-text">
+              <h2 class="plank-title">
+                Затрудняетесь в выборе учебной программы?
+              </h2>
+              <p class="plank-text">
+                Позвольте нашим профессионалам помочь Вам в выборе профиля для
+                вашего ребенка!
+              </p>
+            </div>
+          </div>
+          <div class="btn-wrapper">
+            <button class="btn btn__plank">Бесплатная консультация</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <section class="teachers">
+      <div class="container">
+        <h2 class="section-title">Преподаватели</h2>
+        <p class="section-suptitle">
+          Те самые люди, которые позволяют нам быть теми, кем мы являемся!
+        </p>
+        <div class="teachers-inner row">
+          <div class="teachers-item col-md-4">
+            <div class="teachers-img">
+              <img src="../assets/img/arrow.svg" alt="" />
+            </div>
+            <h4 class="teachers-title">сложное простыми словами</h4>
+            <p class="teachers-text">
+              Тут нужно что-нибудь классное написать побольше о том. что за
+              ступени отбора такие.
+            </p>
+          </div>
+          <div class="teachers-item col-md-4">
+            <div class="teachers-img">
+              <img src="../assets/img/settings.svg" alt="" />
+            </div>
+            <h4 class="teachers-title">5 ступеней отбора Кадров</h4>
+            <p class="teachers-text">
+              Тут нужно что-нибудь классное написать побольше о том. что за
+              ступени отбора такие.
+            </p>
+          </div>
+          <div class="teachers-item col-md-4">
+            <div class="teachers-img">
+              <img src="../assets/img/finger.svg" alt="" />
+            </div>
+            <h4 class="teachers-title">Индивидуальный подход к каждому</h4>
+            <p class="teachers-text">
+              Тут нужно что-нибудь классное написать побольше о том. что за
+              ступени отбора такие.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="teachers-plank">
+        <div class="container">
+          <div class="row teachers-plank-inner">
+            <div class="col-md-3">
+              <h3 class="teachers-plank-title">100+</h3>
+              <p class="teachers-plank-text">учеников прошли наши курсы</p>
+            </div>
+            <div class="col-md-3">
+              <h3 class="teachers-plank-title">8+</h3>
+              <p class="teachers-plank-text">различных учебных программ</p>
+            </div>
+            <div class="col-md-3">
+              <h3 class="teachers-plank-title">20+</h3>
+              <p class="teachers-plank-text">
+                сертифицированных преподавателей
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
     <Whyus />
     <Pricing />
-    <Testimonials/>
-    <Contactus />
-    <Faq/>
-    <Promo />
-    <Form/>
-    <Footer/>
+    <Testimonials />
+    <section class="contact">
+      <div class="container">
+        <div class="contact-inner">
+          <div class="contact-left">
+            <h2 class="contact-left-title">
+              Хотите Начать <br />
+              Обучаться с Нами?
+            </h2>
+            <ul class="contact-left-list">
+              <li class="contact-left-list-item">
+                Подберем учителя индивидуально с учетом уровня и темперамента
+                ученика
+              </li>
+              <li class="contact-left-list-item">
+                Грамотно организуем процесс учебы так, чтобы ребенку
+                действительно нравилось то, что он изучает
+              </li>
+              <li class="contact-left-list-item">
+                На вводном занятии познакомим Вас с репетитором и составим план
+                обучения
+              </li>
+            </ul>
+          </div>
+          <div class="contact-right">
+            <div class="contact-right-img">
+              <img src="../assets/img/call.svg" alt="" />
+            </div>
+            <h3 class="contact-right-title">
+              Начните свой путь в LikeIT уже сегодня!
+            </h3>
+            <p class="contact-right-text">
+              Запишитесь на пробное занятие и дай своему ребенку возможность
+              обучиться профессиям будущего.
+            </p>
+            <button class="btn btn__plank">Получить консультацию</button>
+            <button class="btn btn__plank btn__plank-empty">
+              Написать нам
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+    <Faq />
+    <div class="promo">
+      <div class="container">
+        <div class="row plank-inner">
+          <div class="col-md-9 plnak-content">
+            <div class="plank-img">
+              <img src="../assets/img/cookie2.svg" alt="" />
+            </div>
+            <div class="plnak-content-text">
+              <h2 class="plank-title">Промокод на скидку 15% на все курсы</h2>
+              <p class="plank-text">
+                Не упустите возможность получить максимальную выгоду. Назовите
+                промокод нашему менеджеру и наслаждайтесь лучшими ценами!
+              </p>
+            </div>
+          </div>
+          <div class="btn-wrapper">
+            <div class="btn btn__plank promo-code">Study2020</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Form />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from "@/components/Header.vue";
-import Advantages from "@/components/Advantages.vue";
-import Benefits from "@/components/Benefits.vue";
-import Wellcome from "@/components/Wellcome.vue";
-import About from "@/components/About.vue";
 import Services from "@/components/Services.vue";
-import Plank from "@/components/Plank.vue";
-import Teachers from "@/components/Teachers.vue";
 import Whyus from "@/components/Whyus.vue";
 import Pricing from "@/components/Pricing.vue";
 import Testimonials from "@/components/Testimonials.vue";
-import Contactus from "@/components/Contactus.vue"
-import Faq from "@/components/Faq.vue"
-import Promo from "@/components/Promo.vue"
-import Form from "@/components/Form.vue"
-import Footer from "@/components/Footer.vue"
-
+import Faq from "@/components/Faq.vue";
+import Form from "@/components/Form.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
   components: {
     Header,
-    Advantages,
-    Benefits,
-    Wellcome,
-    About,
     Services,
-    Plank,
-    Teachers,
     Whyus,
     Pricing,
     Testimonials,
-    Contactus,
     Faq,
-    Promo,
     Form,
-    Footer
+    Footer,
   },
 };
 </script>
