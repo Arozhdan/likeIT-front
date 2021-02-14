492d067383e693fc5f6de6b42d99039f95a2f555
Arozhdan Baibussynov <halala30@gmail.com>
2021-02-14 19:10:44 +0100

    ready

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,46 +1,37 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   env: {
-    strapiBaseUri: process.env.API_URL || "http://localhost:1337"
+    strapiBaseUri: process.env.API_URL || "http://localhost:1337",
   },
   head: {
-    title: 'frontend',
+    title: "frontend",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-    '@assets/css/bootstrap-grid.min.css',
-    '@assets/css/main.scss'
-  ],
+  css: ["@assets/css/bootstrap-grid.min.css", "@assets/css/main.scss"],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    
-    { src: '~/plugins/vue-carousel', ssr: false },
-    { src: '~/plugins/carousel', ssr: false },
-    { src: '~/plugins/collapse', ssr: true },
+    { src: "~/plugins/vue-carousel", ssr: false },
+    { src: "~/plugins/carousel", ssr: false },
+    { src: "~/plugins/collapse", ssr: true }
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-  ],
+  buildModules: ["@nuxtjs/dotenv"],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    '@nuxtjs/apollo',
-  ],
-  apollo: {  
+  modules: ["@nuxtjs/apollo"],
+  apollo: {
     clientConfigs: {
       default: {
         httpEndpoint: process.env.BACKEND_URL || "http://localhost:1337/graphql"
@@ -49,6 +40,5 @@ export default {
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
-}
+  build: {}
+};
